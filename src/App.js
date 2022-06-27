a568436e3f4eb7e5aae0fa3b9998649171c0eff3
Yang <j.xiang@campus.unimib.it>
2022-06-28 01:12:22 +0200

    end

--- src/App.js
+++ src/App.js
@@ -1,39 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StreamChat } from 'stream-chat';
-import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import {
+  Attachment,
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  LoadingIndicator,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from 'stream-chat-react';
 
 import 'stream-chat-react/dist/css/index.css';
 
-const chatClient = StreamChat.getInstance('dz5f4d5kzrue');
 const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoibHVja3ktcmVjaXBlLTgiLCJleHAiOjE2NTYzODM5MTN9.LEASxaR2hA09vY2K4CKx6AjwMZ3Z-rOwfwGVjtT3obs';
 
-chatClient.connectUser(
+const filters = { type: 'messaging', members: { $in: ['lucky-recipe-8'] } };
+const sort = { last_message_at: -1 };
+
+const attachments = [
   {
-    id: 'lucky-recipe-8',
-    name: 'lucky',
-    image: 'https://getstream.io/random_png/?id=lucky-recipe-8&name=lucky',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71k0cry-ceL._SL1500_.jpg',
+    name: 'iPhone',
+    type: 'product',
+    url: 'https://goo.gl/ppFmcR',
   },
-  userToken,
-);
+];
 
-const channel = chatClient.channel('messaging', 'custom_channel_id', {
-  // add as many custom fields as you'd like
-  image: 'https://www.drupal.org/files/project-images/react.png',
-  name: 'React Discussion',
-  members: ['lucky-recipe-8'],
-});
+const CustomAttachment = (props) => {
+  const { attachments } = props;
+  const [attachment] = attachments || [];
 
-const App = () => (
-  <Chat client={chatClient} theme='messaging light'>
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader />
-        <MessageList />
-        <MessageInput />
-      </Window>
-      <Thread />
-    </Channel>
-  </Chat>
-);
+  if (attachment?.type === 'product') {
+    return (
+      <div>
+        Product:
+        <a href={attachment.url} rel='noreferrer'>
+          <img alt='custom-attachment' height='100px' src={attachment.image} />
+          <br />
+          {attachment.name}
+        </a>
+      </div>
+    );
+  }
+
+  return <Attachment {...props} />;
+};
+
+const App = () => {
+  const [chatClient, setChatClient] = useState(null);
+
+  useEffect(() => {
+    const initChat = async () => {
+      const client = StreamChat.getInstance('dz5f4d5kzrue');
+
+      await client.connectUser(
+        {
+          id: 'lucky-recipe-8',
+          name: 'lucky',
+          image: 'https://getstream.io/random_png/?id=lucky-recipe-8&name=lucky',
+        },
+        userToken,
+      );
+
+      const [channelResponse] = await client.queryChannels(filters, sort);
+
+      await channelResponse.sendMessage({
+        text: 'Your selected product is out of stock, would you like to select one of these alternatives?',
+        attachments,
+      });
+
+      setChatClient(client);
+    };
+
+    initChat();
+  }, []);
+
+  if (!chatClient) {
+    return <LoadingIndicator />;
+  }
+
+  return (
+    <Chat client={chatClient} theme='messaging light'>
+      <ChannelList filters={filters} sort={sort} />
+      <Channel Attachment={CustomAttachment}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
 
 export default App;
